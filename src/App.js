d972205e9e45808dd77da4454df95e3c8b27e82f
Jesse-Maurice Iyoha <iyohajessy@gmail.com>
2023-09-20 19:32:04 +0100

    routing done

--- src/App.js
+++ src/App.js
@@ -10,12 +10,11 @@ import Main from './Components/Main';
 
 function App() {
   return (
-  
-      <Routes>
-        <Route path='/' element={<SignIn />} />
-        <Route path='/home' element={<Main/>} />
-      </Routes>
-    
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/Login" element={<SignIn />} />
+      <Route path="/home" element={<Main />} />
+    </Routes>
   );
 }
 
