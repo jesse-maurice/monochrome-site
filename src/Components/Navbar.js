9e5b5908b721b1c3b4a1fcd1bf9e393e4f8b5199
Jesse-maurice iyoha <iyohajessy@gmail.com>
2024-06-23 19:58:36 +0100

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -159,6 +159,7 @@ const Navbar = () => {
                       Challenges
                     </a>
                     <a
+                      target="_blank"
                       href="https://monoblog.framer.website/"
                       className="flex items-start px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                     >
